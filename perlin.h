358b6e59311f867d3b9fa068ca23d1049c08e60b
anonymoustoo <34704245+anonymoustoo@users.noreply.github.com>
2017-12-20 20:41:59 +0800

    Update perlin.h

--- perlin.h
+++ perlin.h
@@ -1,3 +1,9 @@
+/*
+This file implements the two dimensional Perlin Noise.
+The source code is obtained from the website at http://mrl.nyu.edu/~perlin/doc/oscar.html.
+In this project,we use the Perlin Noise to generating synthetic DEMs to check the correctness of our proposed variant.
+*/
+
 #ifndef PERLIN_H_DEF
 #define PERLIN_H_DEF
 
